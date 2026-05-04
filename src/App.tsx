8528dbb17e6493a214e4b5d6d7d9a730af525453
AliZ14-ECE <ali.alobadye@gmail.com>
2026-05-04 13:17:35 +0300

    feat: initialize project with React, Vite, and Tailwind CSS
    - Add React and Vite setup with main entry point
    - Create initial sections: About, Contacts, Experience, Hero, Projects, Testimonials
    - Implement global styles with Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Navbar } from "./Layout/Navbar";
+import { Contacts } from "./sections/Contacts";
+import { Experience } from "./sections/Experience";
+import { Hero } from "./sections/Hero";
+import { About } from "./sections/About";
+import { Projects } from "./sections/Projects";
+import { Testimonials } from "./sections/Testimonials";
+
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero/>
+        <About/>
+        <Projects/>
+        <Experience/>
+        <Testimonials/>
+        <Contacts/>        
+      </main>
+    </div>
+  );
+}
+
+export default App;
